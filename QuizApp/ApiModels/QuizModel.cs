965dd509bf08928f2105543462744947d1985b25
TristanHoladay <tholaday777@gmail.com>
2019-08-28 18:18:07 -0600

    QuizModel and QuizMapping Fixed

--- QuizApp/ApiModels/QuizModel.cs
+++ QuizApp/ApiModels/QuizModel.cs
@@ -6,18 +6,10 @@ namespace QuizApp.ApiModels
     
     public class QuizModel
     {
-      public int Id { get; set; }
-     public string Title { get; set; }
-    public string Description { get; set; }
-    public string Instructions { get; set; }
-    public IEnumerable<QuestionModel> Questions { get; set; }
-        public QuizModel(int Id, string Title, string Description, string Instructions, IEnumerable<QuestionModel>Questions)
-        {
-            this.Id = Id;
-            this.Title = Title;
-            this.Description = Description;
-            this.Instructions = Instructions;
-            this.Questions = Questions;
-        }
-}
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Instructions { get; set; }
+        public IEnumerable<QuestionModel> Questions { get; set; }
+    }
 }
