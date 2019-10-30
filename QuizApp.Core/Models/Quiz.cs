f59b6bec4ed6f9cce1a8b8010293c49ef23b5854
Osman Samad <osamad84@yahoo.com>
2019-10-29 19:32:58 -0500

    changes.

--- QuizApp.Core/Models/Quiz.cs
+++ QuizApp.Core/Models/Quiz.cs
@@ -5,5 +5,12 @@ namespace QuizApp.Core.Models
 {
     public class Quiz 
     {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Instuctions { get; set; }
+        ICollection<QuizQuestion> QuizQuestions { get; set; }
     }
+    
 }
+
