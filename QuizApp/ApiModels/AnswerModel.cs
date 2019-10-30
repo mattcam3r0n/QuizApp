b631bc31e914838925c04fa79a0dbca1a4796ed3
RisingPhoenix3333 <wattsty222@gmail.com>
2019-10-29 19:35:33 -0500

    new commit

--- QuizApp/ApiModels/AnswerModel.cs
+++ QuizApp/ApiModels/AnswerModel.cs
@@ -3,11 +3,7 @@ namespace QuizApp.ApiModels
 {
     public class AnswerModel
     {
-        public int Id {get; set; }
-        public string Content {get; set;}
-        public bool IsCorrect {get; set;}
-        public int QuestionId {get; set;}
-        public Question Question {get; set;}
+        
         // TODO: create answer model props
     }
 }
