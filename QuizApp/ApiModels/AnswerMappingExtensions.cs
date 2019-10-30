412fe3db645676ef01ed7e955824421456c69ad6
raygarcia <raymundo.garcia316@yahoo.com>
2019-10-29 20:09:21 -0500

    push experiment

--- QuizApp/ApiModels/AnswerMappingExtensions.cs
+++ QuizApp/ApiModels/AnswerMappingExtensions.cs
@@ -30,6 +30,8 @@ namespace QuizApp.ApiModels
                 QuestionId = answerModel.QuestionId,
                 Content = answerModel.Content,
                 IsCorrect = answerModel.IsCorrect,
+
+                //push experiment
 			};
 		}
 
