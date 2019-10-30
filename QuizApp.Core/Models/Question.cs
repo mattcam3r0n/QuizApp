609097733744875f3c8cf7dea250bc1bb392ea9b
lindsayw89 <lindsay.89@outlook.com>
2019-10-29 19:25:18 -0500

    "question Changes"

--- QuizApp.Core/Models/Question.cs
+++ QuizApp.Core/Models/Question.cs
@@ -6,6 +6,13 @@ namespace QuizApp.Core.Models
 
     public class Question 
     {
+        int Id { get; set; }
+        string QuestionType { get; set; }
+        string Prompt { get; set; }
+        ICollection<Answer> Answers { get; set; }
+        ICollection<QuizQuestion> QuizQuestions { get; set; }
+
+
     }
 }
     
