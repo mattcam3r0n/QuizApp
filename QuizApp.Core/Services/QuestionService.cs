24570546b73cef3f0d529fd0d3f140cc8296951d
lindsayw89 <lindsay.89@outlook.com>
2019-10-31 19:14:50 -0500

    "question service"

--- QuizApp.Core/Services/QuestionService.cs
+++ QuizApp.Core/Services/QuestionService.cs
@@ -5,8 +5,41 @@ using QuizApp.Core.Models;
 
 namespace QuizApp.Core.Services
 {
-    public class QuestionService 
+    public class QuestionService : IQuestionService
     {
         // TODO: Inherit and implement the IQuestionService interface
+        private readonly IQuestionRepository _questionRepository;
+
+        public QuestionService(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public Question Add(Question question)
+        {
+            return _questionRepository.Add(question);
+        }
+        public Question Get(int id)
+        {
+            return _questionRepository.Get(id);
+        }
+        public IEnumerable<Question> GetAll()
+        {
+            return _questionRepository.GetAll();
+        }
+        public Question Update(Question updatedQuestion)
+        {
+            return _questionRepository.Update(updatedQuestion);
+        }
+        public void Remove(int id)
+        {
+            var question=_questionRepository.Get(id);
+            _questionRepository.Remove(question);
+
+
+        }
+
+
+
     }
 }
