571a9d635210d556614f404e9a46c750af41be50
Herb1691 <hwright_1691@hotmail.com>
2020-02-19 21:01:31 -0600

    Question Domain Model Complete

--- QuizApp.Core/Models/Question.cs
+++ QuizApp.Core/Models/Question.cs
@@ -6,6 +6,10 @@ namespace QuizApp.Core.Models
 
     public class Question 
     {
+        public int Id { get; set; }
+        public string QuestionType { get; set; }
+        public string Prompt { get; set; }
+        public IEnumerable<Answer> Answers { get; set; }
     }
 }
     
