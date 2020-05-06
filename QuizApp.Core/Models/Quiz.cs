222823907db19c5c680bac6a77a53803dd2f586b
Scott G <56052999+gsco@users.noreply.github.com>
2020-05-05 20:19:36 -0500

    Add Quiz model.

--- QuizApp.Core/Models/Quiz.cs
+++ QuizApp.Core/Models/Quiz.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizApp.Core.Models
 {
     public class Quiz 
     {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required][MaxLength(200)]
+        public string Title { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+        
+        [Required]
+        public string Instructions { get; set; }
+        
+        public ICollection<QuizQuestion> QuizQuestions { get; set; }
     }
 }
