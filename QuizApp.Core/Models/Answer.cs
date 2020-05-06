b8e4fe6a4c53e277a9b0366f50678bb3d51c9c33
gsanders32 <gray.sanders@gmail.com>
2020-05-05 19:38:39 -0500

    Done

--- QuizApp.Core/Models/Answer.cs
+++ QuizApp.Core/Models/Answer.cs
@@ -6,7 +6,6 @@ namespace QuizApp.Core.Models
         public int Id { get; set; }
         public string Content { get; set; }
         public bool IsCorrect { get; set; }
-
         public int QuestionID { get; set; }
         public Question Question { get; set; }
     }
