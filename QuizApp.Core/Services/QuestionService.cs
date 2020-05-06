ea5a2deaf113f392fee65f153591fd994899fbba
AmyTroutman <amyrtroutman@gmail.com>
2020-05-05 20:21:36 -0500

    Question Service done.

--- QuizApp.Core/Services/QuestionService.cs
+++ QuizApp.Core/Services/QuestionService.cs
@@ -5,8 +5,55 @@ using QuizApp.Core.Models;
 
 namespace QuizApp.Core.Services
 {
-    public class QuestionService 
+    public class QuestionService : IQuestionService
     {
-        // TODO: Inherit and implement the IQuestionService interface
+        private readonly IQuestionRepository _questionRepository;
+
+        public QuestionService(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        public Question Add(Question newQuestion)
+        {
+            if(newQuestion.Answers.Count == 0)
+            {
+                throw new ApplicationException("A question must have an answer.");
+            }
+            else if(newQuestion.Answers.Count > 1)
+            {
+                throw new ApplicationException("A question can only have 1 answer.");
+            }
+            return _questionRepository.Add(newQuestion);
+        }
+
+        public Question Get(int id)
+        {
+            return _questionRepository.Get(id);
+        }
+
+        public IEnumerable<Question> GetAll()
+        {
+            return _questionRepository.GetAll();
+        }
+
+        public void Remove(int id)
+        {
+            var removedQuestion = _questionRepository.Get(id);
+            _questionRepository.Remove(removedQuestion);
+        }
+
+        public Question Update(Question updatedQuestion)
+        {
+            if (updatedQuestion.Answers.Count == 0)
+            {
+                throw new ApplicationException("A question must have an answer.");
+            }
+            else if (updatedQuestion.Answers.Count > 1)
+            {
+                throw new ApplicationException("A question can only have 1 answer.");
+            }
+            return _questionRepository.Update(updatedQuestion);
+        }
     }
 }
